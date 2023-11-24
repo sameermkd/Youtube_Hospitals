a83ffcd8138733cb812da3580e1365b64d5d9d0d
Sameer Kongath <zoomixinfo@gmail.com>
2023-11-24 22:36:04 +0530

    Part 2

--- hospital/views.py
+++ hospital/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Welcome to Django")
+    return render(request,"index.html")
