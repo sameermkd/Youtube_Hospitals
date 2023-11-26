3232826e1defba4cc7f40f04cfa7e2a9be1f54c4
Sameer Kongath <zoomixinfo@gmail.com>
2023-11-26 09:22:17 +0530

    Part 3

--- hospital/models.py
+++ hospital/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Department(models.Model):
+    dep_name=models.TextField(max_length=50)
+    dep_desc=models.TextField(max_length=200)
+    def __str__(self):
+        return self.dep_name
--- hospital/views.py
+++ hospital/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import Department
 
 def index(request):
-    return render(request,"index.html")
+    department=Department.objects.all()
+    return render(request,"index.html",{"department":department})
