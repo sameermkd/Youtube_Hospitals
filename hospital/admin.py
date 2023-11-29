43949b754c32e2d65842a0725ef74f4d29fa4cea
Sameer Kongath <zoomixinfo@gmail.com>
2023-11-29 23:03:13 +0530

    Part 5

--- hospital/admin.py
+++ hospital/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from . models import Department, Doctor
+from . models import Department, Doctor, Booking
 
 admin.site.register(Department)
 admin.site.register(Doctor)
+admin.site.register(Booking)
--- hospital/views.py
+++ hospital/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from . models import Department, Doctor
+from . forms import BookingForm
 
 def index(request):
     department=Department.objects.all()
     doctor=Doctor.objects.all()
+    form=BookingForm()
+    if request.method=="POST":
+        form=BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("Booking Success")
     context = {
         "department":department,
-        "doctor":doctor
+        "doctor":doctor,
+        "form":form
     }
     return render(request,"index.html",context)
